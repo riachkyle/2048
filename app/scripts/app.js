2ecdc199f462954023bf7491e656972d0f1ee248
riachkyle <riachkyle@gmail.com>
2014-11-02 19:11:50 -0800

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name twentyfoutyeightApp
+ * @description
+ * # twentyfoutyeightApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('twentyfoutyeightApp', [
+    'ngCookies'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twentyfoutyeightApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the twentyfoutyeightApp
+ */
+angular.module('twentyfoutyeightApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
